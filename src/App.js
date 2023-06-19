1a599590852580817115cea6b7376ddb25f10bf9
Parth Sagar <kallathiyaparth744@gmail.com>
2023-06-19 17:30:19 +0530

    day_12 _19-June

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react'
-import UseMemo from './components/10-day_17_june/useMemo/UseMemo'
+import UseTransition from './components/12-day_19_june/useTransition/UseTransition'
+// import UseReducer from './components/12-day_19_june/useRedcer/UseReducer'
+// import UseContext from './components/12-day_19_june/useContext/UseContext'
+// import UseCallback from './components/12-day_19_june/useCallback/UseCallback'
+// import UseMemo from './components/10-day_17_june/useMemo/UseMemo'
 // import UseRef from './components/10-day_17_june/useRef/UseRef'
 // import UseNavigate from './components/10-day_17_june/useNavigate/UseNavigate'
 // import UseLayoutEffect from './components/10-day_17_june/useLayout/UseLayout'
@@ -9,10 +13,14 @@ export default function App() {
   return (
     <div>
       {/* <Usestate/> */}
-      {/* <UseLayoutEffect/> */}
+      {/* <UseLayoutEffect/> */} 
       {/* <UseNavigate/> */}
       {/* <UseRef/> */}
-      <UseMemo/>
+      {/* <UseMemo/> */}
+      {/* <UseCallback/> */}
+      {/* <UseContext/> */}
+      {/* <UseReducer/> */}
+      <UseTransition/>
       
     </div>
   )
