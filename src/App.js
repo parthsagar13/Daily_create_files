ff1694f17a1a22594c76871158a73ab849696850
Parth Sagar <kallathiyaparth744@gmail.com>
2023-06-22 19:05:09 +0530

    14-day_21_june again files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+
 import React from 'react'
-import App1 from './components/13-day_20_june/Nested Component/App1'
+// import UseFormik from './components/14-day_21_june/Formik and yup/UseFormik'
+import FormikForms from './components/14-day_21_june/Formik and yup/FormikForms'
+// import App1 from './components/13-day_20_june/Nested Component/App1'
+// import UseEffect from './components/10-day_17_june/useEffect/UseEffect'
 // import NestedpassProps from './components/13-day_20_june/Nested Component/NestedpassProps'
 // import Nested from './components/13-day_20_june/Nested Component/Nested'
 // import UseTransition from './components/12-day_19_june/useTransition/UseTransition'
-// import UseReducer from './components/12-day_19_june/useRedcer/UseReducer'
+// import UseReducer from './componqents/12-day_19_june/useRedcer/UseReducer'
 // import UseContext from './components/12-day_19_june/useContext/UseContext'
 // import UseCallback from './components/12-day_19_june/useCallback/UseCallback'
 // import UseMemo from './components/10-day_17_june/useMemo/UseMemo'
-// import UseRef from './components/10-day_17_june/useRef/UseRef'
+// import UseRef from './components/10-day_17_june/useRef/UseRef' 
 // import UseNavigate from './components/10-day_17_june/useNavigate/UseNavigate'
 // import UseLayoutEffect from './components/10-day_17_june/useLayout/UseLayout'
 // import Usestate from './components/10-day_17_june/usestate/Usestate'
@@ -15,7 +19,9 @@ import App1 from './components/13-day_20_june/Nested Component/App1'
 export default function App() {
   return (
     <div>
+    
       {/* <Usestate/> */}
+      {/* <UseEffect/> */}
       {/* <UseLayoutEffect/> */} 
       {/* <UseNavigate/> */}
       {/* <UseRef/> */}
@@ -26,7 +32,10 @@ export default function App() {
       {/* <UseTransition/> */}
       {/* <Nested/> */}
       {/* <NestedpassProps/> */}
-      <App1/>
+      {/* <App1/> */}
+      <FormikForms/>
+      {/* <UseFormik/> */}
+      {/* <Example/> */}
 
     </div>
   )
