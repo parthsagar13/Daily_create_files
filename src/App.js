9dc446cc2697084f9faf188962c4cc4e00a0b742
Parth Sagar <kallathiyaparth744@gmail.com>
2023-06-23 18:58:36 +0530

    16-day_23_june

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-
-import React from 'react'
+import React from "react";
+import Home from "./components/16-day_23_june/React_Router/Home";
+import { Route, Routes } from "react-router-dom";
+import About from "./components/16-day_23_june/React_Router/About";
+import Navbar from "./components/16-day_23_june/React_Router/Navbar";
+import OrderSummary from "./components/16-day_23_june/React_Router/OrderSummary";
+// import NoMatchRoutes from "./co  mponents/16-day_23_june/React_Router/NoMatchRoutes";
+import FeaturedProduct from "./components/16-day_23_june/React_Router/FeaturedProduct";
+import NewProduct from "./components/16-day_23_june/React_Router/NewProduct";
+import Product from "./components/16-day_23_june/React_Router/Product";
+import User from "./components/16-day_23_june/React_Router/Dynemic_Routes/User";
 // import UseFormik from './components/14-day_21_june/Formik and yup/UseFormik'
-import FormikForms from './components/14-day_21_june/Formik and yup/FormikForms'
+// import FormikForms from './components/14-day_21_june/Formik and yup/FormikForms'
+// import Parth from './components/16-day_23_june/Formik/Parth'
 // import App1 from './components/13-day_20_june/Nested Component/App1'
 // import UseEffect from './components/10-day_17_june/useEffect/UseEffect'
 // import NestedpassProps from './components/13-day_20_june/Nested Component/NestedpassProps'
@@ -10,8 +20,8 @@ import FormikForms from './components/14-day_21_june/Formik and yup/FormikForms'
 // import UseReducer from './componqents/12-day_19_june/useRedcer/UseReducer'
 // import UseContext from './components/12-day_19_june/useContext/UseContext'
 // import UseCallback from './components/12-day_19_june/useCallback/UseCallback'
-// import UseMemo from './components/10-day_17_june/useMemo/UseMemo'
-// import UseRef from './components/10-day_17_june/useRef/UseRef' 
+// import UsxeMemo from './components/10-day_17_june/useMemo/UseMemo'
+// import UseRef from './components/10-day_17_june/useRef/UseRef'
 // import UseNavigate from './components/10-day_17_june/useNavigate/UseNavigate'
 // import UseLayoutEffect from './components/10-day_17_june/useLayout/UseLayout'
 // import Usestate from './components/10-day_17_june/usestate/Usestate'
@@ -19,10 +29,9 @@ import FormikForms from './components/14-day_21_june/Formik and yup/FormikForms'
 export default function App() {
   return (
     <div>
-    
       {/* <Usestate/> */}
       {/* <UseEffect/> */}
-      {/* <UseLayoutEffect/> */} 
+      {/* <UseLayoutEffect/> */}
       {/* <UseNavigate/> */}
       {/* <UseRef/> */}
       {/* <UseMemo/> */}
@@ -33,10 +42,22 @@ export default function App() {
       {/* <Nested/> */}
       {/* <NestedpassProps/> */}
       {/* <App1/> */}
-      <FormikForms/>
+      {/* <FormikForms/> */}
       {/* <UseFormik/> */}
-      {/* <Example/> */}
-
+      {/* <Parth/> */}
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="ordersummary" element={<OrderSummary />} />
+        <Route path="product" element={<Product />}>
+          <Route path="featuredProduct" element={<FeaturedProduct />} />
+          <Route index element={<FeaturedProduct />} />
+          <Route path="newProduct" element={<NewProduct />} />
+        </Route>
+        {/* <Route path="*" element={<NoMatchRoutes />} /> */}
+        <Route path="user" element={<User />}/>
+      </Routes>
     </div>
-  )
+  );
 }
